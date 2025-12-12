ef853bc6c7397708f2e3e18a36cbc575d4cfd58b
TW20022 <118551729+TW20022@users.noreply.github.com>
2025-12-12 19:44:37 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -2,6 +2,8 @@
 
 #移除luci-app-attendedsysupgrade
 sed -i "/attendedsysupgrade/d" $(find ./feeds/luci/collections/ -type f -name "Makefile")
+sed -i "/onionshare-cli/d" $(find ./feeds/luci/collections/ -type f -name "Makefile")
+sed -i "/qmodem/d" $(find ./feeds/luci/collections/ -type f -name "Makefile")
 #修改默认主题
 sed -i "s/luci-theme-bootstrap/luci-theme-$WRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
 #修改immortalwrt.lan关联IP
